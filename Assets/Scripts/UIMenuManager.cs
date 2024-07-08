f679bf54aa84873f51ca23ce3d3a9336eb8beab8
tokwalover666 <91291v@gmail.com>
2024-07-08 23:41:44 +0800

    asdasd

--- Assets/Scripts/UIMenuManager.cs
+++ Assets/Scripts/UIMenuManager.cs
@@ -5,16 +5,14 @@ using DG.Tweening;
 
 public class UIMenuManager : MonoBehaviour
 {
-    public RectTransform mainMenu, settingsMenu, closetMenu;
-    public CanvasGroup settingsCanvasGroup;
+    public RectTransform mainMenu, settingsMenu, closetMenu, petProfileMenu;
+
     public float uiTransitionSpeed;
 
 
     void Start()
     {
-        settingsCanvasGroup.alpha = 0;
-        settingsCanvasGroup.interactable = false;
-        settingsCanvasGroup.blocksRaycasts = false;
+
     }
 
     void Update()
@@ -25,13 +23,23 @@ public class UIMenuManager : MonoBehaviour
     public void ShowClosetMenu()
     {
         closetMenu.DOAnchorPos(Vector2.zero, uiTransitionSpeed, true).SetEase(Ease.InOutElastic);
-        mainMenu.DOAnchorPos(new Vector2(-1100, 0), uiTransitionSpeed, true).SetEase(Ease.Linear);
+        mainMenu.DOAnchorPos(new Vector2(1100, 0), uiTransitionSpeed, true).SetEase(Ease.Linear);
     }
-
-    public void ShowMainMenu()
+    public void ShowPetProfile()
+    {
+        petProfileMenu.DOAnchorPos(Vector2.zero, uiTransitionSpeed, true).SetEase(Ease.InOutElastic);
+        mainMenu.DOAnchorPos(new Vector2(-2130, 0), uiTransitionSpeed, true).SetEase(Ease.Linear);
+    }
+    public void ShowMainMenuFromSettings()
     {
         mainMenu.DOAnchorPos(Vector2.zero, uiTransitionSpeed, true).SetEase(Ease.InOutElastic);
-        settingsMenu.DOAnchorPos(new Vector2(-1100, 0), uiTransitionSpeed, true).SetEase(Ease.Linear);
+        settingsMenu.DOAnchorPos(new Vector2(-1130, 0), uiTransitionSpeed, true).SetEase(Ease.Linear);
+    }
+
+    public void ShowMainMenuFromCloset()
+    {
+        mainMenu.DOAnchorPos(Vector2.zero, uiTransitionSpeed, true).SetEase(Ease.InOutElastic);
+        closetMenu.DOAnchorPos(new Vector2(1130, 0), uiTransitionSpeed, true).SetEase(Ease.Linear);
     }
 
     public void ShowSettingsMenu()
@@ -40,30 +48,36 @@ public class UIMenuManager : MonoBehaviour
         mainMenu.DOAnchorPos(new Vector2(-1100, 0), uiTransitionSpeed, true).SetEase(Ease.Linear);
     }
 
-/*    public void ShowSettings()
+    public void ShowSettingsMenuFromCloset()
     {
-        settingsMenu.SetAsLastSibling();
-
-        Sequence sequence = DOTween.Sequence();
-        sequence.Append(settingsMenu.DOAnchorPos(Vector2.zero, uiTransitionSpeed, true));
-        sequence.Append(settingsMenu.DOScale(Vector2.zero, 0));
-        sequence.AppendInterval(0.1f);
-        sequence.Append(settingsMenu.DOScale(Vector2.one, 0.2f)).SetEase(Ease.Linear);
-
-        settingsCanvasGroup.DOFade(1, uiTransitionSpeed);
-        settingsCanvasGroup.interactable = true;
-        settingsCanvasGroup.blocksRaycasts = true;
-
-        Debug.Log($"Before animation: {settingsMenu.anchoredPosition} / {settingsMenu.localScale}");
-        Debug.Log($"After animation: {settingsMenu.anchoredPosition} / {settingsMenu.localScale}");
+        settingsMenu.DOAnchorPos(Vector2.zero, uiTransitionSpeed, true).SetEase(Ease.InOutElastic);
+        closetMenu.DOAnchorPos(new Vector2(-1100, 0), uiTransitionSpeed, true).SetEase(Ease.Linear);
     }
 
-    public void BackSettings()
-    {
-        Sequence sequence = DOTween.Sequence();
-        sequence.Append(settingsMenu.DOScale(Vector2.zero, 0.1f));
-        sequence.AppendInterval(0.1f);
-        sequence.Append(settingsMenu.DOAnchorPos(new Vector2(-1100, 0), 0, true));
-        sequence.Append(settingsMenu.DOScale(Vector2.one, 0)).SetEase(Ease.Linear);
-    }*/
+    /*    public void ShowSettings()
+        {
+            settingsMenu.SetAsLastSibling();
+
+            Sequence sequence = DOTween.Sequence();
+            sequence.Append(settingsMenu.DOAnchorPos(Vector2.zero, uiTransitionSpeed, true));
+            sequence.Append(settingsMenu.DOScale(Vector2.zero, 0));
+            sequence.AppendInterval(0.1f);
+            sequence.Append(settingsMenu.DOScale(Vector2.one, 0.2f)).SetEase(Ease.Linear);
+
+            settingsCanvasGroup.DOFade(1, uiTransitionSpeed);
+            settingsCanvasGroup.interactable = true;
+            settingsCanvasGroup.blocksRaycasts = true;
+
+            Debug.Log($"Before animation: {settingsMenu.anchoredPosition} / {settingsMenu.localScale}");
+            Debug.Log($"After animation: {settingsMenu.anchoredPosition} / {settingsMenu.localScale}");
+        }
+
+        public void BackSettings()
+        {
+            Sequence sequence = DOTween.Sequence();
+            sequence.Append(settingsMenu.DOScale(Vector2.zero, 0.1f));
+            sequence.AppendInterval(0.1f);
+            sequence.Append(settingsMenu.DOAnchorPos(new Vector2(-1100, 0), 0, true));
+            sequence.Append(settingsMenu.DOScale(Vector2.one, 0)).SetEase(Ease.Linear);
+        }*/
 }
