e763f456c61bb2f5e203999e17f4da08cd9ed45c
tokwalover666 <91291v@gmail.com>
2024-07-09 22:55:18 +0800

    customizer

---
+++ Assets/Scripts/Customizer.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Customizer : MonoBehaviour
+{
+    public SpriteRenderer part;
+    public Sprite[] options;
+    public int index;
+
+    private void Update()
+    {
+        for (int i = 0; i < options.Length; i++)
+        {
+            if(i == index)
+            {
+                part.sprite = options[i];
+            }
+        }
+    }
+
+    public void Swap()
+    {
+        if(index < options.Length - 1)
+        {
+            index++;
+        }
+        else
+        {
+            index = 0;
+        }
+    }
+}
