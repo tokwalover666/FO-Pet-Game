39104066ccb05e0a6421803a51ad64c6281c6643
tokwalover666 <91291v@gmail.com>
2024-07-12 11:57:41 +0800

    cat custom, static index for parts

--- Assets/Scripts/Customizer.cs
+++ Assets/Scripts/Customizer.cs
@@ -4,28 +4,111 @@ public class Customizer : MonoBehaviour
 {
     public SpriteRenderer part;
     public Sprite[] options;
-    public int index;
+    public static int eyesIndex = 0; // Static variables to track indices
+    public static int mouthIndex = 0;
+    public static int itemIndex = 0;
+    private int index;
+    private static bool isInitialized = false;
+
+    private void Start()
+    {
+        if (!isInitialized)
+        {
+            index = 0;
+            isInitialized = true;
+        }
+    }
 
     private void Update()
     {
-        for (int i = 0; i < options.Length; i++)
+        // Set the part sprite based on the static indices
+        if (gameObject.name == "eyes")
         {
-            if(i == index)
+            index = eyesIndex;
+        }
+        else if (gameObject.name == "mouth")
+        {
+            index = mouthIndex;
+        }
+        else if (gameObject.name == "item")
+        {
+            index = itemIndex;
+        }
+
+        part.sprite = options[index];
+    }
+
+    public void SwapRightButton()
+    {
+        if (gameObject.name == "eyes")
+        {
+            if (eyesIndex < options.Length - 1)
             {
-                part.sprite = options[i];
+                eyesIndex++;
+            }
+            else
+            {
+                eyesIndex = 0;
+            }
+        }
+        else if (gameObject.name == "mouth")
+        {
+            if (mouthIndex < options.Length - 1)
+            {
+                mouthIndex++;
+            }
+            else
+            {
+                mouthIndex = 0;
+            }
+        }
+        else if (gameObject.name == "item")
+        {
+            if (itemIndex < options.Length - 1)
+            {
+                itemIndex++;
+            }
+            else
+            {
+                itemIndex = 0;
             }
         }
     }
 
-    public void Swap()
+    public void SwapLeftButton()
     {
-        if(index < options.Length - 1)
+        if (gameObject.name == "eyes")
         {
-            index++;
+            if (eyesIndex > 0)
+            {
+                eyesIndex--;
+            }
+            else
+            {
+                eyesIndex = options.Length - 1;
+            }
         }
-        else
+        else if (gameObject.name == "mouth")
         {
-            index = 0;
+            if (mouthIndex > 0)
+            {
+                mouthIndex--;
+            }
+            else
+            {
+                mouthIndex = options.Length - 1;
+            }
+        }
+        else if (gameObject.name == "item")
+        {
+            if (itemIndex > 0)
+            {
+                itemIndex--;
+            }
+            else
+            {
+                itemIndex = options.Length - 1;
+            }
         }
     }
 }
